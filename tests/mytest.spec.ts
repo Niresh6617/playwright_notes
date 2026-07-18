3a4dc8df74587f96c0e695cc09118ce24f8e528e
Niresh6617 <niresh6617@gmail.com>
2026-07-18 08:03:51 +0530

    first commit

---
+++ tests/mytest.spec.ts
@@ -0,0 +1,31 @@
+// Class - 1 
+// TypeScript - is a superset of JavaScript
+
+// npm - Node Package Manager
+// npx - Node Package Executor 
+
+
+
+// Create a Type Script file name as mytest.spec.ts
+
+// Test , Expect  - Built in Function
+
+// note: Playwright is a Module so we need to import the test and expect 
+
+// • Test Function will be use to create a test cases 
+// •  Expect Function will be used for Assertion or Validation to test 
+
+// the first statement after creating the file is import the test and expect function from playwright Module
+
+// Next we have to write a test case for that we using the test function in that test function has acept two paramaters one is title of the test and another one is a function wheather it could be a simple function, arrow function or annonymous function 
+//Syntax to write test 
+// test ("titile" , () =>{
+
+// // step 1
+// // step 2
+// // step 3
+
+// })
+
+import {test,expect} from "@playwright/test" 
+
